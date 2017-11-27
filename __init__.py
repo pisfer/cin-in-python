e36a3df456ef05c875c5a42467fe10bb87b92480
Gleb <gleb.kor888@gmail.com>
2017-11-27 19:09:26 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,29 @@
+import re
+
+
+class cin:
+    """cin func in python"""
+
+    def __init__(self, *args, n):
+        self.args = args
+        self.n = n
+        self.pora = "(?P<" + self.args[0] + ">.*)"
+        self.index = ""
+        self.vbn = {}
+        for self.g in self.args[1:]:
+            self.pora = self.pora + "\s+(?P<" + self.g + ">.*)"
+        self.i = input(self.n)
+        self.hpo = re.match(self.pora, self.i)
+        if self.hpo:
+            for self.k in self.args:
+                self.vbn[self.k] = self.hpo.group(self.k)
+        else:
+            raise ArgumentError("You inserted false arguments")
+
+    def get(self, index):
+        self.index = index
+        return self.vbn[self.index]
+
+
+if __name__ == '__main__':
+    print(cin("b", "n", n="d").get("b"))
